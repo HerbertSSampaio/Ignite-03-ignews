3ec9cfb1d94c149f6d5550ec13faaa233a30d9c6
HerbertSousa <herberti1405@outlook.com>
2021-04-17 09:58:27 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Inicio | ignews</title>
+    </Head>
+    <h1>Hello World</h1>
+    </>
+  )
+}
